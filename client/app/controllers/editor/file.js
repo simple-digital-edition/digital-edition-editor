134eb061f5608698678a546653f63f6dc9eaf37e
Mark Hall <mark.hall@informatik.uni-halle.de>
2018-07-20 12:02:25 +0200

    Implemented the file editor tabs

--- client/app/controllers/editor/file.js
+++ client/app/controllers/editor/file.js
@@ -4,6 +4,20 @@ export default Controller.extend({
     selected_metadata_node: null,
 
     actions: {
+        'select-tab-panel': function(target, ev) {
+            ev.preventDefault();
+            var panel_root = ev.target.parentElement.parentElement.parentElement.parentElement;
+            panel_root.querySelectorAll('.tabs-title.is-active').forEach(function(item) {
+                item.classList.remove('is-active');
+                item.querySelector('a').removeAttribute('aria-selected');
+            });
+            panel_root.querySelectorAll('.tabs-panel.is-active').forEach(function(item) {
+                item.classList.remove('is-active');
+            });
+            ev.target.parentElement.classList.add('is-active');
+            ev.target.setAttribute('aria-selected', 'true');
+            panel_root.querySelector(target + '.tabs-panel').classList.add('is-active');
+        },
         'select-metadata-node': function(node) {
             this.set('selected_metadata_node', node);
         },
