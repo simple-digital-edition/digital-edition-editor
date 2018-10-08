b137566732c69858245bdc8306b00ab389c46843
Mark Hall <mark.hall@informatik.uni-halle.de>
2018-10-08 11:26:28 +0200

    Strip empty name repositories.

--- server/src/digital_edition_editor/__init__.py
+++ server/src/digital_edition_editor/__init__.py
@@ -7,7 +7,7 @@ def main(global_config, **settings):
     for setting in ['git.repos', 'gitlab.api', 'gitlab.projectid', 'gitlab.token']:
         settings[setting] = dict([(repo2[:repo2.find(':')], repo2[repo2.find(':') + 1:])
                                   for repo1 in settings[setting].split('\n')
-                                  for repo2 in repo1.split(',')])
+                                  for repo2 in repo1.split(',') if repo2.strip()])
     config = Configurator(settings=settings)
     #config.include('.models')
     config.include('.routes')
