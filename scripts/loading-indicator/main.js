498db2908eadd236d8d06416ba7b0b4ef73360c9
Mark Hall <mark.hall@informatik.uni-halle.de>
2019-03-29 19:00:54 +0100

    Remove debug code

--- scripts/loading-indicator/main.js
+++ scripts/loading-indicator/main.js
@@ -5,7 +5,6 @@
 (function() {
     let links = document.querySelectorAll('a[href]');
     for(let idx = 0; idx < links.length; idx++) {
-        console.log(idx);
         links[idx].addEventListener('click', function() {
             document.querySelector('#loading').classList.add('is-visible');
         });
