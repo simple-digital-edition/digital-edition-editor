67d61d0c2999d2ce63f880968947213d5c2e8712
Mark Hall <mark.hall@informatik.uni-halle.de>
2019-04-01 15:00:05 +0100

    Remove old server code

--- server/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export http_proxy="http://192.168.5.200:3128"
-export https_proxy="http://192.168.5.200:3128"
-
-git pull --rebase
-export PIPENV_VENV_IN_PROJECT=True
-pipenv install
-pipenv run pip install -e .
