a296411504ba79eddf816981460aad230aba7b58
Mark Hall <mark.hall@informatik.uni-halle.de>
2019-04-01 14:45:19 +0100

    Implement generating pull requests and local merges

--- editor/models/repository.py
+++ editor/models/repository.py
@@ -5,3 +5,5 @@ class Repository(models.Model):
     name = models.CharField(max_length=250)
     title = models.CharField(max_length=250)
     url = models.CharField(max_length=250)
+    gitlab_api = models.CharField(max_length=250)
+    gitlab_api_token = models.CharField(max_length=250)
--- editor/views/repository.py
+++ editor/views/repository.py
@@ -1,7 +1,9 @@
 import os.path
+import requests
 
 from django.conf import settings
 from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponse
 from django.shortcuts import render
 from git import Repo
 
@@ -45,7 +47,41 @@ def repository(request, rid):
     master_changes = [{'message': commit.message,
                        'author': commit.author.name,
                        'datetime': commit.committed_datetime.strftime('%d %B %Y %H:%M')}
-                      for commit in repo.iter_commits('master..master@{u}')]
+                      for commit in repo.iter_commits('%s..master@{u}' % request.user.username)]
     return render(request, 'editor/repository.jinja2', {'repository': repository,
                                                         'remote_changes': remote_changes,
                                                         'master_changes': master_changes})
+
+
+@permission_required('editor.repository.can_read')
+def pull_request(request, rid):
+    if request.method == 'POST':
+        repository = Repository.objects.get(pk=rid)
+        response = requests.post('%s/merge_requests' % repository.gitlab_api,
+                                 headers={'PRIVATE-TOKEN': repository.gitlab_api_token},
+                                 params={'source_branch': request.user.username,
+                                         'target_branch': 'master',
+                                         'title': 'TEI Editor Changes'})
+        if response.status_code >= 200 and response.status_code < 300:
+            return HttpResponse('', status=200)
+        else:
+            return HttpResponse('', status=500)
+
+
+@permission_required('editor.repository.can_read')
+def local_merge(request, rid):
+    if request.method == 'POST':
+        repository = Repository.objects.get(pk=rid)
+        base_path = os.path.join(settings.REPOSITORY_BASE, str(rid), str(request.user.username))
+        repository = Repo(base_path)
+        # Pull the latest changes from the master branch
+        repository.heads.master.checkout()
+        repository.remotes.origin.pull()
+        repository.heads[request.user.username].checkout()
+        # Merge them in
+        repository.git.merge('master')
+        # Pull any branch changes
+        repository.remotes.origin.pull()
+        # Push all changes
+        repository.remotes.origin.push()
+        return HttpResponse('', status=200)
