7bdd278247d5555a5cd995d4f33b0dae07c9886e
Mark Hall <mark.hall@informatik.uni-halle.de>
2019-04-10 08:32:06 +0200

    Updated the build process

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-export http_proxy="http://192.168.5.200:3128"
-export https_proxy="http://192.168.5.200:3128"
+source local-config
 
-git checkout -- .
 git pull
 export PIPENV_VENV_IN_PROJECT=True
 pipenv install
-pipenv run pip install -e .
