cd32d91de363cc79bb10347721482df83b47ea55
Mark Hall <mark.hall@informatik.uni-halle.de>
2019-04-10 09:20:40 +0200

    Improved and documented the build script

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-source local-config
+# The local-config file can be used to set deployment-specific environment settings, such as proxies
+if [ -f 'local-config' ]
+then
+    source local-config
+fi
 
+# Update the application and install it in an in-project virtualenv
 git pull
 export PIPENV_VENV_IN_PROJECT=True
 pipenv install
+
+# Update the static files
 pipenv run python manage.py collectstatic --settings digital_edition_editor.deploy_settings
+
+# Run any optional server-restart commands
