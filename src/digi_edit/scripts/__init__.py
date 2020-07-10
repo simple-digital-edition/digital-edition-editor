c7431a9f549914a089618d4e80389b67c7c28122
Mark Hall <mark.hall@work.room3b.eu>
2020-07-10 21:50:37 +0100

    Added admin cmd interface

--- src/digi_edit/scripts/__init__.py
+++ src/digi_edit/scripts/__init__.py
@@ -1,1 +1,24 @@
-# package
+import click
+import os
+
+from pyramid.paster import get_appsettings, setup_logging
+
+from .config import create_config
+from .db import init_db
+
+
+@click.group()
+@click.option('-c', '--config', default='production.ini')
+@click.pass_context
+def main(ctx, config):
+    """Administration Utility for the Digital Edition Editor"""
+    try:
+        setup_logging(config)
+        settings = get_appsettings(config)
+        ctx.obj = {'settings': settings}
+    except FileNotFoundError:
+        pass
+
+
+main.add_command(create_config)
+main.add_command(init_db)
