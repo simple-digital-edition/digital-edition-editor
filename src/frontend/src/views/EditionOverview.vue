1fc17deda037ae7ffd408dfedb3e84d75d8df392
Mark Hall <mark.hall@work.room3b.eu>
2020-10-06 12:11:37 +0100

    Highlight the delete icon

--- src/frontend/src/views/EditionOverview.vue
+++ src/frontend/src/views/EditionOverview.vue
@@ -76,7 +76,7 @@
                                     </a>
                                 </router-link>
                                 <a @click="deleteBranch(branch)">
-                                    <svg viewBox="0 0 24 24" class="icon alert">
+                                    <svg viewBox="0 0 24 24" class="icon warning">
                                         <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
                                     </svg>
                                 </a>
