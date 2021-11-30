932da6dc18282ab766527d6fbb3e6e9cce7864d8
Mark Hall <mark.hall@work.room3b.eu>
2021-11-30 16:25:39 +0000

    Simplified the start script

--- docker/base/start_digi_edit.sh
+++ docker/base/start_digi_edit.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-DEFAULT_GATEWAY=`ip route show | grep "default via" | grep -E -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]"`
-
-sed -i -e "s#sqlalchemy.url =.*#sqlalchemy.url = $SQLALCHEMY_URL#g" \
-       -e "s#git.url =.*#git.url = ${GIT_URL}#g" \
-       -e "s#gitlab.token =.*#gitlab.token = ${GITLAB_TOKEN}#g" \
-       -e "s#trusted_proxy =.*#trusted_proxy = ${DEFAULT_GATEWAY}#g" \
-       /etc/digi-edit/production.ini
+#DEFAULT_GATEWAY=`ip route show | grep "default via" | grep -E -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]"`
+#
+#sed -i -e "s#trusted_proxy =.*#trusted_proxy = ${DEFAULT_GATEWAY}#g" \
+#       /etc/digi-edit/production.ini
 
 # Setup the database
 digi_edit -c /etc/digi-edit/production.ini init-db
