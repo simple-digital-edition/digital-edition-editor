7b0886fa04f25f820c0676f33a432cd4b7aca7a8
Mark Hall <mark.hall@work.room3b.eu>
2021-12-03 13:50:41 +0000

    Missing a11y label

--- src/digi_edit/frontend/src/routes/TaskEditor.svelte
+++ src/digi_edit/frontend/src/routes/TaskEditor.svelte
@@ -131,7 +131,7 @@
                     <span class="sr-only">Search files</span>
                     <input bind:value={$fileSearchText} type="search" class="blockblock py-1 pl-2 pr-8 border-l border-b border-r border-solid border-neutral w-full text-base focus:shadow-inner"/>
                 </label>
-                <button class="block absolute right-0 top-1/2 p-2 transform -translate-y-1/2">
+                <button class="block absolute right-0 top-1/2 p-2 transform -translate-y-1/2" aria-label="Search">
                     <svg viewBox="0 0 24 24" class="w-4 h-4">
                         <path fill="currentColor" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
                     </svg>
