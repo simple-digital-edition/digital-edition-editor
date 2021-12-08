0af72ede9fdc7fdf961ce0b8bb99cfedd625d12d
Mark Hall <mark.hall@work.room3b.eu>
2021-12-08 11:02:04 +0000

    Remove the gutzkow-specific stuff

--- docker/start_digi_edit.sh
+++ docker/start_digi_edit.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#DEFAULT_GATEWAY=`ip route show | grep "default via" | grep -E -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]"`
-#
-#sed -i -e "s#trusted_proxy =.*#trusted_proxy = ${DEFAULT_GATEWAY}#g" \
-#       /etc/digi-edit/production.ini
-
 # Setup the database
 digi_edit -c /etc/digi-edit/production.ini init-db
 while [ $? -ne 0 ]
