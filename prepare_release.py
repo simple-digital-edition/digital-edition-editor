0abd70c173bbb508fa8951b47c626b040e2a3a8a
Mark Hall <mark.hall@work.room3b.eu>
2022-01-12 13:25:34 +0000

    v1.0.0b14

--- prepare_release.py
+++ prepare_release.py
@@ -6,7 +6,7 @@ from shutil import rmtree
 from subprocess import run
 
 # Update version numbers
-version = '1.0.0b13'
+version = '1.0.0b14'
 
 with open('pyproject.toml') as in_f:
     lines = in_f.readlines()
