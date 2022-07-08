a4a305daffe00180d2fe1e1abb0277aa41590f84
Mark Hall <mark.hall@work.room3b.eu>
2022-07-08 11:36:50 +0100

    Added database setup

--- digi_edit/cli/admin.py
+++ digi_edit/cli/admin.py
@@ -1,14 +1,29 @@
 """Admin functionality."""
+import asyncio
 import click
+
+from ..models import get_engine, Base
+
+
+async def setup_database(drop_existing: bool) -> None:
+    """Create the database tables."""
+    engine = get_engine()
+    async with engine.begin() as conn:
+        if drop_existing:
+            await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 
 @click.command()
-def setup():
-    pass
+@click.option('--drop-existing', is_flag=True, default=False, help='Whether to remove any existing database content')
+def setup(drop_existing: bool) -> None:
+    """Run the application setup."""
+    asyncio.run(setup_database(drop_existing))
 
 
 @click.group()
-def admin():
+def admin() -> None:
+    """Administer the Digital Edition Editor."""
     pass
 
 
