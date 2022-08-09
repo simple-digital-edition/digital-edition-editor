057519f5372ab1108a50a8df9f9b036b44259f2f
Mark Hall <mark.hall@work.room3b.eu>
2022-08-09 13:38:08 +0100

    Don't automatically create the database

--- docker/start_digi_edit.sh
+++ docker/start_digi_edit.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-# Setup the database
-python -m digi_edit admin setup
-while [ $? -ne 0 ]
-do
-    sleep 10
-    python -m digi_edit admin setup
-done
-
 # Run the web application
 python -m digi_edit app server
